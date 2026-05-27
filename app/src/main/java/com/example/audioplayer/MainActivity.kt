05bbc3d2875d59045df699a48877f1861940d89a
dimaijevskiy1999 <dimaijevskiy1999@gmail.com>
2026-05-27 21:29:27 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/audioplayer/MainActivity.kt
+++ app/src/main/java/com/example/audioplayer/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.audioplayer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -11,6 +12,9 @@ import com.example.audioplayer.ui.theme.AudioPlayerTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
+        enableEdgeToEdge()
+        
         setContent {
             AudioPlayerTheme {
                 val viewModel: PlayerViewModel = viewModel()
