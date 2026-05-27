ab9f084b9c7dfb05780cd4c1051d0b07ccf52fbb
dimaijevskiy1999 <dimaijevskiy1999@gmail.com>
2026-05-27 21:36:05 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    id("com.android.application") version "8.2.0" apply false
+    // FIXED: Поднята версия AGP для совместимости с compileSdk 35
+    id("com.android.application") version "8.5.2" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
 }
